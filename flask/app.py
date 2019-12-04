e6630c19fc595b63381dd664b402eafa272c5b05
TSerup <au575786@uni.au.dk>
2019-12-04 22:11:24 +0100

    corrections

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 app = Flask(__name__)
 
@@ -7,14 +6,4 @@ def hello_world():
     return 'Flask Dockerized by Thomas'
 	
 if __name__ == '__main__':
-=======
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized by Thomas'
-	
-if __name__ == '__main__':
->>>>>>> 4c7f03ef6b0aa6e474dd380893c4fb3dca724074
     app.run(debug=True,host='0.0.0.0')
